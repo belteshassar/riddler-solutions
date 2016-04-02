765116c53e555ccd31f7048085f48dc6df524d25
Daniel Eriksson <daniel@deriksson.se>
2016-04-02 16:42:36 +0200

    Add Python solution

---
+++ 20160402/solution.py
@@ -0,0 +1,69 @@
+class PossiblePairs(list):
+    @staticmethod
+    def from_set(s, allow_equal=True):
+        s = set(s)
+        if allow_equal:
+            return PossiblePairs(
+                (i, j) for i in s for j in s if i <= j)
+        else:
+            return PossiblePairs(
+                (i, j) for i in s for j in s if i < j)
+
+    @staticmethod
+    def from_range(*args, allow_equal=True):
+        return PossiblePairs.from_set(range(*args), allow_equal)
+
+    def does_pete_know(self, answer):
+        p = dict()
+        for i, j in self:
+            if i*j in p:
+                p[i*j].append((i, j))
+            else:
+                p[i*j] = [(i, j)]
+        ret = []
+        for pairs in p.values():
+            if (len(pairs) > 1) != (answer == 'yes'):
+                ret += pairs
+        return PossiblePairs(ret)
+
+    def does_susan_know(self, answer):
+        s = dict()
+        for i, j in self:
+            if i+j in s:
+                s[i+j].append((i, j))
+            else:
+                s[i+j] = [(i, j)]
+        ret = []
+        for pairs in s.values():
+            if (len(pairs) > 1) != (answer == 'yes'):
+                ret += pairs
+        return PossiblePairs(ret)
+
+
+if __name__ == '__main__':
+    possible = PossiblePairs.from_range(1, 10)
+    # Q1
+    print(possible
+          .does_pete_know('no')
+          .does_susan_know('no')
+          .does_pete_know('no')
+          .does_susan_know('no')
+          .does_pete_know('no')
+          .does_susan_know('no')
+          .does_pete_know('no')
+          .does_susan_know('no')
+          .does_pete_know('yes')
+          )
+    # Q2
+    print(possible
+          .does_pete_know('no')
+          .does_susan_know('no')
+          .does_pete_know('no')
+          .does_susan_know('no')
+          .does_pete_know('no')
+          .does_susan_know('no')
+          .does_pete_know('no')
+          .does_susan_know('no')
+          .does_pete_know('no')
+          .does_susan_know('yes')
+          )
