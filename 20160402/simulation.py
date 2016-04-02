c826454206d10603b0d7bcdac5764b099b506b8d
Daniel Eriksson <daniel@deriksson.se>
2016-04-02 18:11:27 +0200

    Change class name to `ImpossiblePuzzle`

--- 20160402/simulation.py
+++ 20160402/simulation.py
@@ -1,46 +1,46 @@
-from solution import PossiblePairs
+from solution import ImpossiblePuzzle
 
 
 def tester(*args):
-    def sums(possible):
+    def sums(puzzle):
         ret = dict()
-        for i, j in possible:
+        for i, j in puzzle:
             if i+j in ret:
                 ret[i+j].append((i, j))
             else:
                 ret[i+j] = [(i, j)]
         return ret
 
-    def prods(possible):
+    def prods(puzzle):
         ret = dict()
-        for i, j in possible:
+        for i, j in puzzle:
             if i*j in ret:
                 ret[i*j].append((i, j))
             else:
                 ret[i*j] = [(i, j)]
         return ret
 
-    for a, b in PossiblePairs.from_range(*args):
-        possible = PossiblePairs.from_range(*args)
+    for a, b in ImpossiblePuzzle.from_range(*args):
+        puzzle = ImpossiblePuzzle.from_range(*args)
         p = a*b
         s = a+b
 
         k = 0
         while True:
             k += 1
-            l = len(possible)
-            if len(prods(possible)[p]) == 1:
+            l = len(puzzle)
+            if len(prods(puzzle)[p]) == 1:
                 yield (a, b), 'P{}'.format(k)
                 break
-            possible = possible.does_pete_know('no')
-            if len(sums(possible)[s]) == 1:
+            puzzle = puzzle.does_pete_know('no')
+            if len(sums(puzzle)[s]) == 1:
                 yield (a, b), 'S{}'.format(k)
                 break
-            possible = possible.does_susan_know('no')
-            if l == len(possible):
+            puzzle = puzzle.does_susan_know('no')
+            if l == len(puzzle):
                 yield (a, b), 'Never'
                 break
 
 
 if __name__ == '__main__':
-    print(list(tester(1, 10)))
+    print(list(tester(1, 40)))
--- 20160402/solution.py
+++ 20160402/solution.py
@@ -1,17 +1,17 @@
-class PossiblePairs(list):
+class ImpossiblePuzzle(list):
     @staticmethod
     def from_set(s, allow_equal=True):
         s = set(s)
         if allow_equal:
-            return PossiblePairs(
+            return ImpossiblePuzzle(
                 (i, j) for i in s for j in s if i <= j)
         else:
-            return PossiblePairs(
+            return ImpossiblePuzzle(
                 (i, j) for i in s for j in s if i < j)
 
     @staticmethod
     def from_range(*args, allow_equal=True):
-        return PossiblePairs.from_set(range(*args), allow_equal)
+        return ImpossiblePuzzle.from_set(range(*args), allow_equal)
 
     def does_pete_know(self, answer):
         p = dict()
@@ -24,7 +24,7 @@ class PossiblePairs(list):
         for pairs in p.values():
             if (len(pairs) > 1) != (answer == 'yes'):
                 ret += pairs
-        return PossiblePairs(ret)
+        return ImpossiblePuzzle(ret)
 
     def does_susan_know(self, answer):
         s = dict()
@@ -37,13 +37,13 @@ class PossiblePairs(list):
         for pairs in s.values():
             if (len(pairs) > 1) != (answer == 'yes'):
                 ret += pairs
-        return PossiblePairs(ret)
+        return ImpossiblePuzzle(ret)
 
 
 if __name__ == '__main__':
-    possible = PossiblePairs.from_range(1, 10)
+    puzzle = ImpossiblePuzzle.from_range(1, 10)
     # Q1
-    print(possible
+    print(puzzle
           .does_pete_know('no')
           .does_susan_know('no')
           .does_pete_know('no')
@@ -55,7 +55,7 @@ if __name__ == '__main__':
           .does_pete_know('yes')
           )
     # Q2
-    print(possible
+    print(puzzle
           .does_pete_know('no')
           .does_susan_know('no')
           .does_pete_know('no')
